9d516b0db76f52dbe4336b8febe53131445d01d2
Gautier Ducurtil <gaducurt@z2r3p1.42lyon.fr>
2025-10-15 16:22:10 +0200

    only main

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,40 +6,30 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 10:53:28 by gaducurt          #+#    #+#             */
-/*   Updated: 2025/10/15 15:55:50 by gaducurt         ###   ########.fr       */
+/*   Updated: 2025/10/15 16:19:55 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
-
-class	Megaphone
-{
-	public:
-	Megaphone(int ac, char **input)
-	{
-		if (ac == 1)
-			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		if (ac > 1)
-		{
-			int	i = 1;
-			while (input[i])
-			{
-				int	j = 0;
-				while (input[i][j])
-				{
-					std::cout << (char)::toupper(input[i][j]);
-					j++;
-				}
-				i++;
-			}
-			std::cout << "\n";
-		}
-	};
-};
 
 int main(int ac, char **av)
 {
-    Megaphone   output(ac, av);
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	if (ac > 1)
+	{
+		int	i = 1;
+		while (av[i])
+		{
+			int	j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)::toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << "\n";
+	}
     return (0);
 }
